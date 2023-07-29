5e1a1dbd018ab49ba179c0f246b9ff62424b06f6
3210jr <3210jr@gmail.com>
2023-07-29 03:51:03 -0700

    update test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,22 @@
-const { defaults: tsjPreset } = require("ts-jest/presets")
+const {defaults: tsjPreset} = require('ts-jest/presets');
 
 module.exports = {
   ...tsjPreset,
-  preset: "jest-expo",
+  preset: 'jest-expo',
   verbose: true,
   // collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-  coverageReporters: ["text-summary", "lcov"],
-  testEnvironment: { url: "http://localhost/" },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+  ],
+  coverageReporters: ['text-summary', 'lcov'],
+  testEnvironment: {url: 'http://localhost/'},
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       babelConfig: true,
     },
-    "__DEV__": true,
-    "__TEST__": true
+    __DEV__: true,
+    __TEST__: true,
   },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
@@ -28,14 +31,19 @@ module.exports = {
     ],
   },
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation|@nozbe/.*|@react-navigation/.*|@unimodules/.*|watermelon|react-native-code-push)",
+    '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation|@nozbe/.*|@react-navigation/.*|@unimodules/.*|watermelon|react-native-code-push)',
     // '<rootDir>/node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|react-navigation|rollbar-react-native|@fortawesome|@react-native|@react-navigation|@unimodules/.*|@nozbe/*|react-native-code-push)',
   ],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "/detox", "@react-native"],
-  testEnvironment: "jsdom",
-  setupFiles: ["<rootDir>/test/setup.ts"],
-  "setupFilesAfterEnv": ["@testing-library/jest-native/extend-expect"],
-  "moduleNameMapper": {
-    "\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/imageMock.tsx"
-  }
-}
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '/detox',
+    '@react-native',
+  ],
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/test/setup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/imageMock.tsx',
+  },
+};
